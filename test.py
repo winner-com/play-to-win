f35595558aea21796ced9dc9bb79014c45a62635
Hmangat Kunwar <wwwwinnercom11@gmail.com>
2024-03-02 12:37:54 +0545

    mini calculator

---
+++ test.py
@@ -0,0 +1,12 @@
+number1=int(input("Enter the value of no."))
+number2=int(input("Enter a value of no."))
+add = number1 + number2
+print("the sum of", number1, "and", number2, "is", add)
+sub= number1-number2
+print("the difference of", number1, "and", number2, "is", sub)
+multiplication=number1*number2
+print("the product of", number1, "and", number2, "is", multiplication)
+div=number1/number2
+print("the division of", number1, "and", number2, "is", div)
+modulus=number1%number2
+print("the remainder of",number1, "and", number2, "is", modulus )
